1a46ad72e47285bcb262602c1b16f499c149d4ab
andy <2602047255@qq.com>
2021-01-21 22:08:49 +0800

    完成首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,15 @@
 $.ajaxPrefilter(function(options) {
   options.url = 'http://www.liulongbin.top:3007' + options.url;
+  if(options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    };
+  }
+  options.complete = function(result) {
+    var data = result.responseJSON;
+    if(data.status === 1 && data.message === '身份认证失败！') {
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function() {
+  loadUserInfo();
+  let layer = layui.layer;
+
+  // 退出登录
+  $('#logout').on('click', function() {
+    layer.confirm('确认退出登录？', {icon: 3, title:'提示'}, function(index){
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+      layer.close(index);
+    });
+  })
+});
+
+// 获取用户信息
+function loadUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    success: function (result) {
+      if(result.status !== 0) return layui.layer.msg(result.message);
+      renderAvatar(result.data);
+    }
+  });
+}
+
+// 渲染用户信息
+function renderAvatar(data) {
+  var name = data.nickname || data.username;
+  var avatar = data.user_pic;
+  $('#welcome').html(`欢迎&nbsp;&nbsp;${name}`);
+  if(avatar !== null) {
+    $('.layui-nav-img').attr('src', avatar).show();
+    $('.text-avatar').hide();
+  } else {
+    var firstName = name[0].toUpperCase();
+
+    $('.layui-nav-img').hide();
+    $('.text-avatar').html(firstName).show();
+  }
+}
